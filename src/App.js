c2b2f5ff207315e9fc85f9d502581bdca2b4f853
MehdiRidmani <mahdiridmani22@gmail.com>
2023-06-24 11:09:18 +0100

    c

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
 
-const App = () => {
+function App() {
   return (
-    <div className='App'>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' />
-          <Route path='/cart' />
-        </Routes>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
 export default App;
